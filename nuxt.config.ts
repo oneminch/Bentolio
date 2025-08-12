2496398d0acdfb8e01d454f84b7c098bd133bd30
oneminch <27751688+oneminch@users.noreply.github.com>
2025-08-12 15:28:53 -0400

    fix: use relative path for css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,39 +2,39 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	app: {
-		rootAttrs: {
-			id: "app",
-		},
-		head: {
-			link: [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],
-		},
-	},
+  app: {
+    rootAttrs: {
+      id: "app"
+    },
+    head: {
+      link: [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }]
+    }
+  },
 
-	build: {
-		transpile: ["gsap"],
-	},
+  build: {
+    transpile: ["gsap"]
+  },
 
-	colorMode: { classSuffix: "" },
+  colorMode: { classSuffix: "" },
 
-	compatibilityDate: "2025-05-15",
+  compatibilityDate: "2025-05-15",
 
-	css: ["~/assets/css/main.css"],
+  css: ["app/assets/css/main.css"],
 
-	devtools: { enabled: true },
+  devtools: { enabled: true },
 
-	modules: [
-		"@nuxt/eslint",
-		"@nuxt/icon",
-		"@nuxtjs/color-mode",
-		"nuxt-og-image",
-	],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxtjs/color-mode",
+    "nuxt-og-image"
+  ],
 
-	routeRules: {
-		"/*": { prerender: true },
-	},
+  routeRules: {
+    "/*": { prerender: true }
+  },
 
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
